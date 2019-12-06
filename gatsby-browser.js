1c088c14c592eb7c5c29b69b1055b05ea2ea4444
goliveros <gian.oliveros20@gmail.com>
2019-12-06 14:40:05 +0800

    debug

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,7 @@ import ApolloClient from "apollo-boost"
 import fetch from "isomorphic-fetch"
 
 const client = new ApolloClient({
-  uri: "https://test-fetch.netlify.com/.netlify/functions/graphql",
+  uri: "https://test-function.netlify.com/.netlify/functions/graphql",
   // request: () => alert(),
 })
 
