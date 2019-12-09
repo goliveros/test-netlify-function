df36d08906a01c999b843eed8a44596ff06d4d86
goliveros <gian.oliveros20@gmail.com>
2019-12-09 10:40:04 +0800

    test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,8 +4,13 @@ import ApolloClient from "apollo-boost"
 import fetch from "isomorphic-fetch"
 
 const client = new ApolloClient({
-  uri: "https://test-fetch.netlify.com/.netlify/functions/graphql",
-  // request: () => alert(),
+  uri: "/.netlify/functions/graphql",
+  fetchOptions: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  },
 })
 
 export const wrapRootElement = ({ element }) => (
