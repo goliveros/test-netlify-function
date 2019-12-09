92be92d0b8bf40cf2e545ad8324b7acb05503e3b
goliveros <gian.oliveros20@gmail.com>
2019-12-09 12:30:14 +0800

    test

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -18,7 +18,7 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || "World"} Gians`,
+    hello: (_, { name }) => `Hello ${name || "World"} Gian`,
     getPerson: async (_, { id }) => {
       const response = await axios
         .get(`https://graphql-apollo-server.firebaseio.com/people/${id}.json`)
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,18 +2,16 @@ import React from "react"
 import { ApolloProvider } from "react-apollo"
 import ApolloClient from "apollo-boost"
 import fetch from "isomorphic-fetch"
-
 const client = new ApolloClient({
   uri: "/.netlify/functions/graphql",
-  fetchOptions: {
+  fetch,
+  headers: {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type",
     },
   },
-  fetch,
 })
-
 export const wrapRootElement = ({ element }) => (
   <ApolloProvider client={client}>{element}</ApolloProvider>
 )
