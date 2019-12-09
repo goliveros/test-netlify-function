3e80c95301732982e2a494fbdcfab16fde236581
goliveros <gian.oliveros20@gmail.com>
2019-12-09 10:33:44 +0800

    test

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,20 +1,47 @@
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "Content-Type",
+const { ApolloServer, gql } = require("apollo-server-lambda")
+const axios = require("axios").default
+
+const typeDefs = gql`
+  type Query {
+    hello(name: String): String!
+    getPerson(id: Int!): Person
+  }
+  type Person {
+    name: String
+    hair_color: String
+    skin_color: String
+    eye_color: String
+    birth_year: Int
+    gender: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || "World"} Gian`,
+    getPerson: async (_, { id }) => {
+      // return `this is the passed: ${id}`
+      const response = await axios
+        .get(`https://graphql-apollo-server.firebaseio.com/people/${id}.json`)
+        .then(res => {
+          console.log(res)
+          return res.data
+        })
+        .catch(err => console.log(err))
+      return response
+    },
+  },
 }
 
-exports.handler = async (event, context) => {
-  try {
-    const subject = event.queryStringParameters.name || "Worlds"
-    return {
-      statusCode: 200,
-      headers,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
-  }
-}
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
+    allowedHeaders: "Content-Type, Origin, Accept, X-Requested-With",
+    methods: "GET, PUT, POST",
+  },
+})
