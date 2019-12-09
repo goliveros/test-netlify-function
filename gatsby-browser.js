fb5a69339bfcb20c5588e43142f81160e6040fb2
goliveros <gian.oliveros20@gmail.com>
2019-12-09 12:38:50 +0800

    test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,16 +2,18 @@ import React from "react"
 import { ApolloProvider } from "react-apollo"
 import ApolloClient from "apollo-boost"
 import fetch from "isomorphic-fetch"
+
 const client = new ApolloClient({
   uri: "/.netlify/functions/graphql",
   fetch,
-  headers: {
+  fetchOptions: {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type",
     },
   },
 })
+
 export const wrapRootElement = ({ element }) => (
   <ApolloProvider client={client}>{element}</ApolloProvider>
 )
