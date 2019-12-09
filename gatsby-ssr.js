b7461c4d29b7e6f06f6930212c9cb4afd904926f
goliveros <gian.oliveros20@gmail.com>
2019-12-09 09:52:30 +0800

    sad

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,7 +4,7 @@ import ApolloClient from "apollo-boost"
 import fetch from "isomorphic-fetch"
 
 const client = new ApolloClient({
-  uri: "https://test-fetch.netlify.com/.netlify/functions/graphql",
+  uri: "/.netlify/functions/graphql",
   fetchOptions: {
     headers: {
       "Access-Control-Allow-Origin": "*",
